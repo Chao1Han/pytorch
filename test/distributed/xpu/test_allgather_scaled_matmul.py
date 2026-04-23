ad60322ef4787d303b2c41ee42ba08250035013d
Cherry Zhang <cherry.zhang@intel.com>
2026-04-23 16:02:44 +0800

    Update test_allgather_scaled_matmul.py

--- test/distributed/xpu/test_allgather_scaled_matmul.py
+++ test/distributed/xpu/test_allgather_scaled_matmul.py
@@ -56,7 +56,8 @@ e4m3_type = torch.float8_e4m3fn
 
 def test_allgather_scaled_matmul(rank, world_size):
     torch.xpu.set_device(rank)
-    torch.use_deterministic_algorithms(True, warn_only=True)
+    # todo: only need for accuracy check, otherwise performance loss
+    # torch.use_deterministic_algorithms(True, warn_only=True)
 
     group = dist.group.WORLD
     gather_dim = 0
